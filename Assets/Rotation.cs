bca542a138b26a2699365de5599e2404e07ad153
Owen-Millward <1400942@abertay.ac.uk>
2017-03-17 15:20:23 +0000

    Light parts of arena rotate

---
+++ Assets/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	public float scale;
+
+	void Update () {
+
+		transform.Rotate(Vector3.up, scale * Time.deltaTime);
+	}
+}
